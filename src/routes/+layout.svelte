0cba42d0650547638e9cee5754f5c1950dc4ef0a
kanaksar <kanaksar@students.zhaw.ch>
2025-05-14 13:43:58 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    let {children} = $props()
+    import './styles.css'
+
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/users">Users</a>
+    <a href="/contacts">Contacts</a>
+</nav>
+<div class="container">
+{@render children()}
+</div>
